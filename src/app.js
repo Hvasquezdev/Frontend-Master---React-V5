ac4dcfaa3191906c4cb93ced90d85fb51b441f60
Hector Vasquez <hvasquezdev@gmail.com>
2020-04-22 23:47:57 -0400

    converting to JSX, configuring ESLint for React

--- src/app.js
+++ src/app.js
@@ -1,26 +1,16 @@
-import React from 'react';
-import { render } from 'react-dom';
-import Pet from './Pet';
+import React from "react";
+import { render } from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Rocky",
-      animal: "Dog",
-      breed: "Dalmatian",
-    }),
-    React.createElement(Pet, {
-      name: "Peppe",
-      animal: "Bird",
-      breed: "Cockatiel",
-    }),
-    React.createElement(Pet, {
-      name: "Doing",
-      animal: "Cat",
-      breed: "Mixed",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Rocky" animal="Dog" breed="Dalmatian" />
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel" />
+      <Pet name="Doing" animal="Cat" breed="Mixed" />
+    </div>
+  );
 };
 
-render(React.createElement(App), document.querySelector("#root"));
+render(<App />, document.querySelector("#root"));
