32d44a2d5171ae472cd10ecbb42611b9a7fc3908
Hector Vasquez <hvasquezdev@gmail.com>
2020-04-23 23:05:57 -0400

    useState, custome hooks, update ESLint, bring in petfinder client

---
+++ src/SearchParams.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { ANIMALS } from "@frontendmasters/pet";
+import useDropdown from "./hooks/useDropdown";
+
+const SearchParams = () => {
+  const [location, setLocation] = useState("Bolívar, Venezuela");
+  const [breeds, setBreeds] = useState([]);
+  const [animal, AnimalDropdown] = useDropdown("Animal", "dog", ANIMALS);
+  const [breed, BreedDropdown] = useDropdown("Breed", "", breeds);
+
+  return (
+    <div className="search-params">
+      <form>
+        <label htmlFor="location">
+          Location
+          <input
+            id="location"
+            value={location}
+            onChange={(e) => setLocation(e.target.value)}
+            placeholder="Location"
+          />
+        </label>
+        <AnimalDropdown />
+        <BreedDropdown />
+        <button>Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default SearchParams;
